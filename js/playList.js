98aeb6fe6f7fa0dab1735c996435f705e256884c
Roman Shevyakov <roman@mgstr.ru>
2023-02-21 10:41:02 +0300

    feat: make 1-6 tasks of momentum

---
+++ js/playList.js
@@ -0,0 +1,29 @@
+const playList = [
+    {
+      title: 'Aqua Caelestis',
+      src: './assets/sounds/Aqua Caelestis.mp3',
+      duration: '00:58'
+    },
+    {
+      title: 'Darius CHERIE',
+      src: './assets/sounds/Darius CHERIE.mp3',
+      duration: '03:58'
+    },
+    {
+      title: 'Leven Kali LET IT RAIN',
+      src: './assets/sounds/Leven Kali LET IT RAIN.mp3',
+      duration: '00:58'
+    },
+    {
+      title: 'Richard J Aarden Not This Time',
+      src: './assets/sounds/Richard J Aarden Not This Time.mp3',
+      duration: '02:58'
+    },
+    {
+      title: 'River Flows In You',
+      src: './assets/sounds/River Flows In You.mp3',
+      duration: '03:50'
+    }
+]
+
+export default playList;
