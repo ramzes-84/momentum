7172c03e9638e3326fcd96eb305eaade33324a94
Roman Shevyakov <roman@mgstr.ru>
2023-02-24 18:41:47 +0300

    feat: total progress 108 points of 150

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ import playList from './playList.js';
 const config = {
   lang: 'ru',
   defaultCity: 'Шымкент',
-  imgSrc: 'github'
+  isPlay: false,
+  playNum: 0,
+  blocks: ['time', 'date', 'greeting-container', 'quote-container', 'weather', 'player'],
 }
 const time = document.querySelector('.time');
 const dateToday = document.querySelector('.date');
@@ -81,19 +83,20 @@ const weatherDescription = document.querySelector('.weather-description');
 const wind = document.querySelector('.wind');
 const humidity = document.querySelector('.humidity');
 const city = document.body.querySelector('.city');
-city.value = config.defaultCity;
+city.value = (localStorage.getItem('city') || config.defaultCity);
 const weatherError = document.body.querySelector('.weather-error');
 const names = document.body.querySelector('.names');
 const audio = new Audio();
 const playBtn = document.body.querySelector('.play');
 const playNext = document.body.querySelector('.play-next');
 const playPrev = document.body.querySelector('.play-prev');
+const soundOnOff = document.body.querySelector('.sound-off');
 const rus = document.body.querySelector('.rus');
 const eng = document.body.querySelector('.eng');
+const settingsBtn = document.body.querySelector('.settings-icon');
+const settingsBlock = document.body.querySelector('.settings-container');
 let currentTimeOfTheDay;
 let numOfBckgrndImg = getRandomNum();
-let isPlay = false;
-let playNum = 0;
 window.addEventListener('beforeunload', setLocalStorage);
 window.addEventListener('load', getLocalStorage);
 bkgnBtnNext.addEventListener('click', getSlideNext);
@@ -105,11 +108,70 @@ playNext.addEventListener('click', turnNextSong);
 playPrev.addEventListener('click', turnPrevSong);
 rus.addEventListener('click', () => translate('ru'));
 eng.addEventListener('click', () => translate('en'));
+audio.addEventListener('ended', turnNextSong);
+soundOnOff.addEventListener('click', toggleSound);
+settingsBtn.addEventListener('click', showSettings);
+
+function setListenerToConfigs() { //Вешает слушатель на инпуты в настроках
+  config.blocks.forEach(function (item) {
+  let element = document.body.querySelector('input[name=' + item + ']');
+  element.addEventListener('change', () => blockOnOff(item));
+    }
+  )
+}
+
+function blockOnOff(str) {
+  let block = document.querySelector('.' + str);
+  block.classList.toggle('hidden');
+}
+
+function showSettings() {
+  settingsBlock.classList.toggle('hidden')
+}
+
+function toggleSound() {
+  if (audio.volume === 1) audio.volume = 0;
+  else audio.volume = 1;
+}
+
+function turnNextSong() {
+  if (config.playNum < (playList.length - 1)) {
+    config.playNum += 1;
+  } else {
+    config.playNum = 0;
+  }
+  config.isPlay = !config.isPlay
+  playAudio();
+}
+
+function turnPrevSong() {
+  if (config.playNum > 0) {
+    config.playNum -= 1;
+  } else {
+    config.playNum = playList.length - 1;
+  }
+  config.isPlay = !config.isPlay
+  playAudio();
+}
+
+function playAudio() {
+  audio.src = playList[config.playNum].src;
+  audio.currentTime = 0;
+  if (!config.isPlay) {
+    audio.play();
+    playBtn.classList.add('pause');
+  } else {
+    audio.pause();
+    playBtn.classList.remove('pause');
+  }
+  config.isPlay = !config.isPlay
+  markCurrTrack()
+}
 
 function markCurrTrack() {
   let trackList = document.body.querySelectorAll('.play-item');
   trackList.forEach(element => element.classList.remove('play-now'));
-  trackList[playNum].classList.add('play-now');
+  trackList[config.playNum].classList.add('play-now');
 }
 
 function translate(lang) {
@@ -144,39 +206,6 @@ function makeSongList() {
     li.textContent = item['title'];
     ulForSongs.append(li);
   }
-}
-
-function turnNextSong() {
-  if (playNum < (playList.length - 1)) {
-    playNum += 1;
-  } else {
-    playNum = 0;
-  }
-  playAudio();
-}
-
-function turnPrevSong() {
-  if (playNum > 0) {
-    playNum -= 1;
-  } else {
-    playNum = playList.length - 1;
-  }
-  playAudio();
-}
-
-function playAudio() {
-  audio.src = playList[playNum].src;
-  audio.currentTime = 0;
-  if (!isPlay) {
-    audio.play();
-    isPlay = true;
-    playBtn.classList.add('pause');
-  } else {
-    audio.pause();
-    isPlay = false;
-    playBtn.classList.remove('pause');
-  }
-  markCurrTrack()
 }
 
 function getQuote() {
@@ -258,7 +287,6 @@ function getLocalStorage() {
     if(localStorage.getItem('city')) {
       city.value = localStorage.getItem('city');
     }
-
 }
 
 function getRandomNum() {
@@ -270,3 +298,49 @@ setBg(numOfBckgrndImg);
 getWeather();
 getQuote();
 makeSongList();
+setListenerToConfigs();
+
+console.log(`Ваша оценка - 108 баллов 
+Не выполненные/не засчитанные пункты:
+1) добавлен прогресс-бар в котором отображается прогресс проигрывания 
+2) при перемещении ползунка прогресс-бара меняется текущее время воспроизведения трека 
+3) над прогресс-баром отображается название трека 
+4) отображается текущее и общее время воспроизведения трека 
+5) добавлен регулятор громкости, при перемещении ползунка регулятора громкости меняется громкость проигрывания звука 
+6) можно запустить и остановить проигрывания трека кликом по кнопке Play/Pause рядом с ним в плейлисте 
+7) переводятся настройки приложения, при переключении языка приложения в настройках, язык настроек тоже меняется 
+8) в качестве источника изображений может использоваться Unsplash API 
+9) в качестве источника изображений может использоваться Flickr API 
+10) в настройках приложения можно указать источник получения фото для фонового изображения: коллекция изображений GitHub, Unsplash API, Flickr API 
+11) если источником получения фото указан API, в настройках приложения можно указать тег/теги, для которых API будет присылает фото 
+12) настройки приложения сохраняются при перезагрузке страницы 
+13) ToDo List - список дел (как в оригинальном приложении) или Список ссылок (как в оригинальном приложении) или Свой собственный дополнительный функционал, по сложности аналогичный предложенным 
+
+Выполненные пункты:
+1) время выводится в 24-часовом формате, например: 21:01:00 
+2) время обновляется каждую секунду - часы идут. Когда меняется одна из цифр, остальные при этом не меняют своё положение на странице (время не дёргается) 
+3) выводится день недели, число, месяц, например: "Воскресенье, 16 мая" / "Sunday, May 16" / "Нядзеля, 16 траўня" 
+4) текст приветствия меняется в зависимости от времени суток (утро, день, вечер, ночь). Проверяется соответствие приветствия текущему времени суток 
+5) пользователь может ввести своё имя. При перезагрузке страницы приложения имя пользователя сохраняется 
+6) ссылка на фоновое изображение формируется с учётом времени суток и случайного номера изображения (от 01 до 20). Проверяем, что при перезагрузке страницы фоновое изображение изменилось. Если не изменилось, перезагружаем страницу ещё раз 
+7) изображения можно перелистывать кликами по стрелкам, расположенным по бокам экрана.Изображения перелистываются последовательно - после 18 изображения идёт 19 (клик по правой стрелке), перед 18 изображением идёт 17 (клик по левой стрелке) 
+8) изображения перелистываются по кругу: после двадцатого изображения идёт первое (клик по правой стрелке), перед 1 изображением идёт 20 (клик по левой стрелке) 
+9) при смене слайдов важно обеспечить плавную смену фоновых изображений. Не должно быть состояний, когда пользователь видит частично загрузившееся изображение или страницу без фонового изображения. Плавную смену фоновых изображений не проверяем: 1) при загрузке и перезагрузке страницы 2) при открытой консоли браузера 3) при слишком частых кликах по стрелкам для смены изображения 
+10) при перезагрузке страницы приложения указанный пользователем город сохраняется, данные о нём хранятся в local storage 
+11) для указанного пользователем населённого пункта выводятся данные о погоде, если их возвращает API. Данные о погоде включают в себя: иконку погоды, описание погоды, температуру в °C, скорость ветра в м/с, относительную влажность воздуха в %. Числовые параметры погоды округляются до целых чисел 
+12) выводится уведомление об ошибке при вводе некорректных значений, для которых API не возвращает погоду (пустая строка или бессмысленный набор символов) 
+13) при загрузке страницы приложения отображается рандомная цитата и её автор 
+14) при перезагрузке страницы цитата обновляется (заменяется на другую). Есть кнопка, при клике по которой цитата обновляется (заменяется на другую) 
+15) при клике по кнопке Play/Pause проигрывается первый трек из блока play-list, иконка кнопки меняется на Pause 
+16) при клике по кнопке Play/Pause во время проигрывания трека, останавливается проигрывание трека, иконка кнопки меняется на Play 
+17) треки пролистываются по кругу - после последнего идёт первый (клик по кнопке Play-next), перед первым - последний (клик по кнопке Play-prev) 
+18) трек, который в данный момент проигрывается, в блоке Play-list выделяется стилем 
+19) после окончания проигрывания первого трека, автоматически запускается проигрывание следующего. Треки проигрываются по кругу: после последнего снова проигрывается первый. 
+20) есть кнопка звука при клике по которой можно включить/отключить звук 
+21) переводится язык и меняется формат отображения даты 
+22) переводится приветствие и placeholder 
+23) переводится прогноз погоды в т.ч описание погоды и город по умолчанию 
+24) переводится цитата дня т.е цитата отображается на текущем языке приложения. Сама цитата может быть другая 
+25) в настройках приложения можно указать язык приложения (en/ru или en/be)  
+26) в настройках приложения можно скрыть/отобразить любой из блоков, которые находятся на странице: время, дата, приветствие, цитата дня, прогноз погоды, аудиоплеер, список дел/список ссылок/ваш собственный дополнительный функционал 
+27) Скрытие и отображение блоков происходит плавно, не влияя на другие элементы, которые находятся на странице, или плавно смещая их`)
